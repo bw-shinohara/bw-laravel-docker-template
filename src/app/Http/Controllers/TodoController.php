3471ab619ff8b504c9e8e51c077a3d3c2d831293
Your Name <you@example.com>
2025-07-08 23:38:24 +0900

    create : DBに登録されているデータを取得する

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -3,12 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Todo;
 
 class TodoController extends Controller
 {
     public function index()
     {
-        return view('todo.index'); // 修正
+        // 追加
+        $todo = new Todo();
+        $todos = $todo->all();
+        dd($todos);
+
+        return view('todo.index'); 
     }
     
 }
