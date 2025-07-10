1087a3f7193b8a0dadbdec28a35307d781fecc16
Your Name <you@example.com>
2025-07-11 00:53:28 +0900

    fix : 画面が真っ白なので修正中

--- src/resources/views/todo/index.blade.php
+++ src/resources/views/todo/index.blade.php
@@ -30,6 +30,11 @@
       <div class="container">
         <div class="row justify-content-center">
           <div class="col-md-8">
+             <!-- ここから -->
+             <p class="text-left">
+               <a class="btn btn-success" href="">ToDoを追加</a>
+             </p>
+             <!-- ここまで -->
             <div class="card">
               <div class="card-header">
                 ToDo一覧
