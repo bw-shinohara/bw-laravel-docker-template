48b3cd4cdcc51bb4c018339c58bcf4f8063b7e35
Your Name <you@example.com>
2025-07-11 22:48:23 +0900

    fix : 画面に表示されないトラブル解決

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -9,11 +9,9 @@ class TodoController extends Controller
 {
     public function index()
     {
-        // 追加
         $todo = new Todo();
         $todos = $todo->all();
         // dd($todos);
-
         return view('todo.index', ['todos' => $todos]); 
     }
     
