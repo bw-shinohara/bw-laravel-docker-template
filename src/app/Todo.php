3bae17cb900a1f1654cc675b3629ee610885afa7
Your Name <you@example.com>
2025-07-12 00:31:30 +0900

    refactor:一括代入（Mass Assignment）

--- src/app/Todo.php
+++ src/app/Todo.php
@@ -6,6 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class Todo extends Model
 {
-    // 追加
     protected $table = 'todos';
+
+    // 追記
+    protected $fillable = [
+        'content',
+    ];
 }
