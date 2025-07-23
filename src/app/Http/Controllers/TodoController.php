7c45cc33d5be326b48c412206042e9d2c03aad2d
Your Name <you@example.com>
2025-07-23 17:25:43 +0900

    new : 編集画面の表示

--- src/app/Http/Controllers/TodoController.php
+++ src/app/Http/Controllers/TodoController.php
@@ -40,4 +40,11 @@ class TodoController extends Controller
 
         return view('todo.show', ['todo' => $todo]);
     }
+
+    public function edit($id)
+{
+        $todo = $this->todo->find($id);
+
+        return view('todo.edit', ['todo' => $todo]);
+}
 }
